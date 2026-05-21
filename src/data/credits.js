f7142026419979c2011f715d41e7b3daba4b01b0
Tom Testu <57745536+Sneyko@users.noreply.github.com>
2026-05-21 19:02:23 +0200

    chore: mise à jour des crédits pour Tom Testu

--- src/data/credits.js
+++ src/data/credits.js
@@ -2,7 +2,7 @@ export const creditsData = [
     { name: "Yanis ACHKAR", role: "Recherche documentaire + rédaction" },
     { name: "Malo BENAGRIA", role: "Recherche documentaire + rédaction" },
     { name: "William GUERRE-CHALEY", role: "Maquettage + rédaction" },
-    { name: "Tom TESTU", role: "Recherche + maquettage" },
+    { name: "Tom TESTU", role: "Recherche + maquettage + montage interview" },
     { name: "Groupe entier", role: "Interviews, sondage, analyse des résultats, choix éditoriaux, relecture" },
     { name: "Cadre du projet", role: "R2.13 Expression Communication - BUT Informatique S2 - IUT Paul Sabatier Toulouse - Année 2025-2026" }
 ];
